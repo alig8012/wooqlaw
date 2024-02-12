2e91f16faeb80446da3952ae30ce5a29d00ce45f
Muhammd Ali <m.ali@kartoa.com>
2024-02-12 11:42:34 +0500

    update

---
+++ public/a-1javascript.js
@@ -0,0 +1,93 @@
+// fullName = "Kartoa";
+// console.log(fullName);
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+
+// console.log("wooqlaw.com");
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+    
+// alert("me");
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+
+// var number1 = 34;
+// var number2 = 56;
+// console.log(number1 + number2);
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+
+// const accountId = 1122
+// let accountEmail = "abc@google.com"
+// accountCity = "Islamabad"
+// let accountState;
+// console.table([accountId, accountEmail, accountCity, accountState])
+
+// console.log(accountId);
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+
+// Data Types (Primitives in Js)
+// Number => 123
+// BigInt => Object
+// String => ""
+// Boolean => true/false
+// null => Object
+// undefined => undefined
+// Symbol => unique
+// Object => Object
+
+// Typeof
+// console.log(typeof "");
+// console.log(typeof 123);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof Object);
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+
+// Data Type (Primitives in Js)
+// let a = null;
+// let b = 123;
+// let c = true; //can also be false
+// let d = BigInt("50") + BigInt("20")
+// let e = "Ali"
+// let f = Symbol("I am symble")
+// let g = undefined;
+// console.log(a, b, c, d, e, f, g)
+// console.log(typeof b)
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+
+// Data Type (Non Primitives in Js) (Object)
+// const item = {
+//     "Ali": true,
+//     "Ahmed": false,
+//     "Hanzala": 123,
+//     "Faisal": undefined
+// }
+// console.log(item["Hanzala"]);
+// console.log(typeof item);
+// console.log(typeof item["Hanzala"]);
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+
+// Data Type (Non Primitives in Js) (Object)
+// const student = {
+//     fullName: "Ali",
+//     age: 30,
+//     cgpa: 8.2,
+//     isPass: true,
+// };
+// console.log(student["fullName"]);
+// console.log(student.fullName);
+
+// student["age"] = student["age"] + 5;
+// console.log(student["age"]);
+
+// student["name"] = "Ahmed";
+// console.log(student["name"]);
+
+/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+
+
