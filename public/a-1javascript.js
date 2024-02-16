15bc7f117944cee5d1bd2645300bdc42bff05ee5
Muhammd Ali <m.ali@kartoa.com>
2024-02-16 17:54:16 +0500

    update

--- public/a-1javascript.js
+++ public/a-1javascript.js
@@ -1,189 +1,11 @@
+/* >>>>>>>>>>>>>> Javascript >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> Veriables >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> fullName is a Veriables & "Kartoa" is a Value >>>>>>>>>>>>>> */
+
 // fullName = "Kartoa";
 // console.log(typeof fullName);
 
-/* >>>>>>>>>>>>>> Important Function >>>>>>>>>>>>>> */
-
-// console.log("wooqlaw.com");    
-// alert("Kartoa");
-// prompt("Kartoa","Your Comments Here");
-// confirm("Login");
-// document.write("wellcome");
-
-/* >>>>>>>>>>>>>> Operators >>>>>>>>>>>>>> */
-/* >>>>>>>>>>>>>> Artimetic Operators >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-
-// console.log("a + b = ", a + b);
-// console.log("a - b = ", a - b);
-// console.log("a / b = ", a / b);
-// console.log("a * b = ", a * b);
-// console.log("a % b = ", a % b);
-// console.log("a ** b = ", a ** b);
-
-/* >>>>>>>>>>>>>> Unary Operators >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-// a = a + 1;
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> or shortcut a++ >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-// a++;
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-// a = a - 1;
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> or shortcut a-- >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-// a--;
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> or shortcut a++ >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-// console.log("a++ = ", a++);
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> or shortcut ++a >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-// console.log("++a = ", ++a);
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> or shortcut a-- >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-// console.log("a-- = ", a--);
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> or shortcut --a >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a = ", a, " & b = ", b);
-// console.log("--a = ", --a);
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> Operators >>>>>>>>>>>>>> */
-/* >>>>>>>>>>>>>> Asignment Operators a += 4; a -= 4; a *= 4; a /= 4; a %= 4; a **= 4; >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// a += 4;
-// console.log("a = ", a);
-
-/* >>>>>>>>>>>>>> Comparison Operators >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 2;
-
-// console.log("5 == 2", a == b);
-// console.log("5 != 2", a != b);
-// console.log("5 === 2", a === b);
-// console.log("5 !== 2", a !== b);
-// console.log("5 > 2", a > b);
-// console.log("5 < 2", a < b);
-
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = 5;
-
-// console.log("5 >= 5", a >= b);
-// console.log("5 <= 5", a <= b);
-
-/* >>>>>>>>>>>>>> Comparison Operators ( 1st "5" string to convert in -> number ) >>>>>>>>>>>>>> */
-
-// let a = 5;
-// let b = "5";
-
-// console.log("a == b", a == b);
-// console.log("a === b", a === b);
-// console.log("a !== b", a !== b);
-
-/* >>>>>>>>>>>>>> Logical Operators ( && ) >>>>>>>>>>>>>> */
-
-// let a = 6;
-// let b = 5;
-
-// let cond1 = a > b;
-// let cond2 = a === 6;
-// console.log("cond1 && cond2 = ", cond1 && cond2);
-
-/* >>>>>>>>>>>>>> ********** let a = 6 to let cond2 = a === 4; >>>>>>>>>>>>>> */
-
-// let a = 6;
-// let b = 5;
-
-// let cond1 = a > b;
-// let cond2 = a === 4;
-// console.log("cond1 && cond2 = ", cond1 && cond2);
-
-/* >>>>>>>>>>>>>> ********** let a = 6; to let cond1 = a < b; >>>>>>>>>>>>>> */
-
-// let a = 6;
-// let b = 5;
-
-// let cond1 = a < b;
-// let cond2 = a === 6;
-// console.log("cond1 && cond2 = ", cond1 && cond2);
-
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
-
-// let a = 6;
-// let b = 5;
-
-// console.log("cond1 && cond2 = ", a < b && a === 6);
-
-/* >>>>>>>>>>>>>> Logical Operators ( || ) Pipe Sign -> || >>>>>>>>>>>>>> */
-
-// let a = 6;
-// let b = 5;
-
-// console.log("cond1 || cond2 = ", a < b || a === 6);
-
-/* >>>>>>>>>>>>>> Logical Operators ( ! ) true to false and false to true >>>>>>>>>>>>>> */
-
-// let a = 6;
-// let b = 5;
-
-// console.log("!(6<5) = ", !(a < b));
-// console.log("!(6<5) = ", !(a === 6));
-
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> let, const & var >>>>>>>>>>>>>> */
 
 // const accountId = 1122
 // let accountEmail = "abc@google.com"
@@ -193,28 +15,19 @@
 
 // console.log(accountId);
 
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> 7 Data Types (Primitives in Js) >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>>
+Number => 123
+BigInt => Object
+String => ""
+Boolean => true/false
+null => Object
+undefined => undefined
+Symbol => unique
+>>>>>>>>>>>>>> */
 
-// Data Types (Primitives in Js)
-// Number => 123
-// BigInt => Object
-// String => ""
-// Boolean => true/false
-// null => Object
-// undefined => undefined
-// Symbol => unique
-// Object => Object
+/* >>>>>>>>>>>>>> 7 Data Types (Primitives in Js) >>>>>>>>>>>>>> */
 
-// Typeof
-// console.log(typeof "");
-// console.log(typeof 123);
-// console.log(typeof undefined);
-// console.log(typeof null);
-// console.log(typeof Object);
-
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
-
-// Data Type (Primitives in Js)
 // let a = null;
 // let b = 123;
 // let c = true; //can also be false
@@ -225,9 +38,17 @@
 // console.log(a, b, c, d, e, f, g)
 // console.log(typeof b)
 
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> Typeof >>>>>>>>>>>>>> */
 
-// Data Type (Non Primitives in Js) (Object)
+// console.log(typeof "");
+// console.log(typeof 123);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof Object);
+
+/* >>>>>>>>>>>>>> Data Type (Non Primitives in Js) >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> Object >>>>>>>>>>>>>> */
+
 // const item = {
 //     "Ali": true,
 //     "Ahmed": false,
@@ -238,9 +59,9 @@
 // console.log(typeof item);
 // console.log(typeof item["Hanzala"]);
 
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> Data Type (Non Primitives in Js) >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> Object >>>>>>>>>>>>>> */
 
-// Data Type (Non Primitives in Js) (Object)
 // const student = {
 //     fullName: "Ali",
 //     age: 30,
@@ -256,6 +77,256 @@
 // student["name"] = "Ahmed";
 // console.log(student["name"]);
 
-/* >>>>>>>>>>>>>> ********** >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> Important Function >>>>>>>>>>>>>> */
 
+// console.log("wooqlaw.com");
+// document.write("wellcome");
+// alert("Kartoa");
+// prompt("Kartoa","Your Comments Here");
+// confirm("Login");
+
+/* >>>>>>>>>>>>>> Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> 1-Artimetic Operators >>>>>>>>>>>>>> */
+ 
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a / b = ", a / b);
+// console.log("a * b = ", a * b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+/* >>>>>>>>>>>>>> Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> 1-Artimetic Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> Increment - Decrement >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> Unary Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> a = a + 1 or shortcut a++ >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// a = a + 1;
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> a = a + 1 or shortcut a++ >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> shortcut a++ >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// a++;
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> a = a - 1 or shortcut a-- >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// a = a - 1;
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> a = a - 1 or shortcut a-- >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> shortcut a-- >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// a--;
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> a++ >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// console.log("a++ = ", a++);
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> ++a >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// console.log("++a = ", ++a);
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> a-- >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> --a >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// console.log("--a = ", --a);
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> 2-Asignment Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> a += 4; a -= 4; a *= 4; a /= 4; a %= 4; a **= 4; >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// a += 4;
+// console.log("a = ", a);
+
+/* >>>>>>>>>>>>>> 3-Comparison Operators >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5 == 2", a == b);
+// console.log("5 != 2", a != b);
+// console.log("5 === 2", a === b);
+// console.log("5 !== 2", a !== b);
+// console.log("5 > 2", a > b);
+// console.log("5 < 2", a < b);
+
+/* >>>>>>>>>>>>>> 1st "5" string to convert in -> number >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = "5";
+
+// console.log("a == b", a == b);
+// console.log("a === b", a === b);
+// console.log("a !== b", a !== b);
+
+/* >>>>>>>>>>>>>> 5 >= 5 , 5 <= 5 >>>>>>>>>>>>>> */
+
+// let a = 5;
+// let b = 5;
+
+// console.log("5 >= 5", a >= b);
+// console.log("5 <= 5", a <= b);
+
+/* >>>>>>>>>>>>>> 4-Logical Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> ( && ) >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> let cond1 = a > b , let cond2 = a === 6 >>>>>>>>>>>>>> */
+
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;
+// let cond2 = a === 6;
+// console.log("cond1 && cond2 = ", cond1 && cond2);
+
+/* >>>>>>>>>>>>>> let cond1 = a > b , let cond2 = a === 4 >>>>>>>>>>>>>> */
+
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;
+// let cond2 = a === 4;
+// console.log("cond1 && cond2 = ", cond1 && cond2);
+
+/* >>>>>>>>>>>>>> let cond1 = a < b , let cond2 = a === 6 >>>>>>>>>>>>>> */
+
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a < b;
+// let cond2 = a === 6;
+// console.log("cond1 && cond2 = ", cond1 && cond2);
+
+/* >>>>>>>>>>>>>> let cond1 = a < b , let cond2 = a === 6 >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> or shortcut >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> a < b && a === 6 >>>>>>>>>>>>>> */
+
+// let a = 6;
+// let b = 5;
+
+// console.log("cond1 && cond2 = ", a < b && a === 6);
+
+/* >>>>>>>>>>>>>> 4-Logical Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> ( || ) Pipe Sign -> || >>>>>>>>>>>>>> */
+
+// let a = 6;
+// let b = 5;
+
+// console.log("cond1 || cond2 = ", a < b || a === 6);
+
+/* >>>>>>>>>>>>>> 4-Logical Operators >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> ( ! ) true to false and false to true >>>>>>>>>>>>>> */
+
+// let a = 6;
+// let b = 5;
+
+// console.log("!(6<5) = ", !(a < b));
+// console.log("!(6<5) = ", !(a === 6));
+
+/* >>>>>>>>>>>>>> Conditional Statements >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> 1- if Statements >>>>>>>>>>>>>> */
+
+// let age = 25;
+
+// if (age >= 18) {
+//     console.log("you can vote");
+// }
+
+// if (age < 18) {
+//     console.log("you cannot vote");
+// }
+
+/* >>>>>>>>>>>>>> 1- if Statements >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> dark mode apply in ( DOM ) >>>>>>>>>>>>>> */
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+
+// if (mode === "light") {
+//     color = "white";
+// }
+
+// console.log(color);
+
+/* >>>>>>>>>>>>>> Conditional Statements >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> 2- if-else Statements >>>>>>>>>>>>>> */
+
+// let age = 25;
+
+// if (age >= 18) {
+//     console.log("vote");
+// }
+
+// else {
+//     console.log("not vote");
+// }
+
+/* >>>>>>>>>>>>>> 2- if-else Statements >>>>>>>>>>>>>> */
+/* >>>>>>>>>>>>>> dark mode apply in ( DOM ) >>>>>>>>>>>>>> */
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+
+// else {
+//     color = "white";
+// }
+
+// console.log(color);
 
